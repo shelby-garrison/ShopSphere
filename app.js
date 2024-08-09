44e3cbe3866f21026e29f6a08b12c0da47797ee9
shelby-garrison <111844173+shelby-garrison@users.noreply.github.com>
2024-08-09 17:47:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,11 @@ app.use(
 );
 app.use(flash());
 // app.use(express.static(path.join(__dirname, "public")));
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
+// app.use(express.static(__dirname + "public"));
+
+
 
 app.use("/", indexRouter);
 app.use("/owners", ownersRouter);
